0b803a69b702b1d36b1d3e024502412eda5607c6
Suprovat007 <suprohazra4@gmail.com>
2024-07-03 01:13:28 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { logo, lock, hamburgerMenu, close } from "../assets";
+
+const Navbar = () => {
+  const [toggle, settoggle] = useState(false);
+  const handleclick = () => settoggle(!toggle);
+  return (
+    <div className="w-full h-20 bg-white sticky top-0 z-[1000]">
+      <div className=" md:max-w-[1480px] max-w-[680px] m-auto w-full h-full flex justify-between items-center md:px-0 px-4">
+        <img src={logo} className=" h-6 cursor-pointer" />
+        <div className=" hidden md:flex items-center">
+          <ul className=" flex gap-4">
+            <li>Home</li>
+            <li>About</li>
+            <li>Support</li>
+            <li>Platform</li>
+            <li>Pricing</li>
+          </ul>
+        </div>
+
+        <div className=" hidden md:flex">
+          <button className=" flex justify-between items-center bg-transparent px-6 gap-2  font-semibold">
+            <img src={lock} />
+            Login
+          </button>
+          <button className=" px-8 py-3 bg-[#44bcbe] rounded-md text-white font-bold">
+            Sign Up For Free
+          </button>
+        </div>
+        <div className=" md:hidden " onClick={handleclick}>
+          <img src={toggle ? close : hamburgerMenu} />
+        </div>
+
+        <div className={ toggle ? 'absolute top-0 z-10 mt-5 p-4 bg-white w-full px-8 md:hidden border-b': 'hidden'}>
+          <ul>
+            <li className=" p-4 hover:bg-gray-100">Home</li>
+            <li className=" p-4 hover:bg-gray-100">About</li>
+            <li className=" p-4 hover:bg-gray-100">Support</li>
+            <li className=" p-4 hover:bg-gray-100">Platform</li>
+            <li className=" p-4 hover:bg-gray-100">Pricing</li>
+          </ul>
+          <div className=" flex flex-col my-4 gap-4">
+            <button className=" border border-[#208486] flex justify-center items-center bg-transparent p-6 gap-2 py-4">
+              <img src={lock} />
+              Login
+            </button>
+            <button className=" px-8 py-5 rounded-md bg-[#208486] text-white font-bold ">
+              Sign Up For Free
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
